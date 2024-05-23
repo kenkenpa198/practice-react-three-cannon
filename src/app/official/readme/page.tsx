e7951a953f26422782aef61a0b1805ca6ba128f4
kenkenpa198 <kenkenpa198@gmail.com>
2024-05-24 07:16:28 +0900

    add plane

--- src/app/official/readme/page.tsx
+++ src/app/official/readme/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Debug, Physics, useBox } from "@react-three/cannon";
+import { Debug, Physics, useBox, usePlane } from "@react-three/cannon";
 import { Canvas } from "@react-three/fiber";
 
+// 箱のコンポーネント
 function Cube(props) {
-  // オブジェクトの接触面に適した形状を選択する。
-  // 箱、平面、球など。それに質量 (mass) も与える。
+  // オブジェクトの接触面に適した形状を選択する。箱、平面、球など。それに質量 (mass) も与える。
+  // 他に初期位置 (position) を指定している。
   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props }));
 
   return (
@@ -13,6 +14,18 @@ function Cube(props) {
     // これで重力や物理世界内の他のオブジェクトの影響を受けるようになる。
     <mesh ref={ref}>
       <boxGeometry />
+    </mesh>
+  );
+}
+
+// 平面 (地面) コンポーネント
+function Plane(props) {
+  // usePlane フックを使用して物理エンジン内で平面の形状を作成する。
+  // 回転を指定して水平に配置している。
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  return (
+    <mesh ref={ref}>
+      <planeGeometry args={[100, 100]} />
     </mesh>
   );
 }
@@ -30,6 +43,7 @@ function App() {
         {/* Physics には物理関連のオブジェクトを入れる */}
         <Physics>
           <Cube />
+          <Plane />
         </Physics>
       </Canvas>
     </div>
